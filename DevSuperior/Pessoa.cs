64cc5713dee463a3929d2ac7448e5c3984f2fab2
Juliano <JulianoMarthins@Yahoo.com.br>
2024-06-22 14:27:37 -0300

    Adicionar arquivos de projeto.

---
+++ DevSuperior/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSuperior {
+    internal class Pessoa {
+
+        public string nome;
+        public string email;
+
+
+        public Pessoa(string nome, string email) {
+            this.nome = nome;
+            this.email = email;
+        }
+
+    }
+}
---
+++ DevSuperior/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSuperior {
+    internal class Program {
+        static void Main(string[] args) {
+                      
+
+            Console.Write("Quantos quartos deseja reservar: ");
+            int num = int.Parse(Console.ReadLine());
+
+
+            Pessoa[] reservas = new Pessoa[10];
+
+            Console.WriteLine(reservas.Length);
+
+            for (int i = 1; i <= num; i++) {
+
+                Console.Write("\nNome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Número do quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                reservas[quarto] = new Pessoa(nome, email);
+
+            }
+
+
+            Console.WriteLine("\nQuartos reservados: ");
+
+
+            for(int i = 1; i <= reservas.Length; i++) {
+                if(reservas[i] != null) {
+                    Console.WriteLine($"Quarto {i}: {reservas[i].nome}, {reservas[i].email}");
+
+                }
+                
+             
+            }
+
+
+        }
+    }
+}
